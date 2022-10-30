88b9da0eb83b21c3dabf07df33155db63fc0935e
JHansen <47265397+UserJHansen@users.noreply.github.com>
2022-10-30 16:59:14 +1000

    Switch to AprilTags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/AutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/AutoRed.java
@@ -6,11 +6,8 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.teamcode.drive.components.AprilDetection;
 import org.firstinspires.ftc.teamcode.drive.components.Bot;
-import org.firstinspires.ftc.teamcode.drive.components.Detection;
-
-import java.util.List;
 
 @Autonomous(group = "advanced")
 public class AutoRed extends LinearOpMode {
@@ -18,44 +15,46 @@ public class AutoRed extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // Declare your drive class
         Bot drive = new Bot(hardwareMap);
-        Detection detection = new Detection(drive.getCamera(), hardwareMap);
+        AprilDetection aprilDetection = new AprilDetection(drive.getCamera(), hardwareMap);
 
         // Right side of red alliance, almost exactly facing the cone
         Pose2d startPose = new Pose2d(35, -66, Math.toRadians(90));
 
         drive.setPoseEstimate(startPose);
 
-        waitForStart();
+        while (!isStarted()) {
+            aprilDetection.update(telemetry);
+            sleep(20);
+        }
 
         if (isStopRequested()) return;
 
-        List<Recognition> detectedCones = detection.getRecognitions();
-        int zone = 2;
+        Comparable<Integer> detected = aprilDetection.getDetected();
 
-        // If we can't detect any cones, just go to the default position
-        if (detectedCones.size() != 0) {
-            Recognition cone = detectedCones.get(0);
-            zone = Integer.parseInt(cone.getLabel().substring(0, 1));
+        if (detected == null) {
+            telemetry.addData("No April tag detected", "");
+            telemetry.update();
+
+            detected = 2;
         }
-
 
         Trajectory traj = drive.trajectoryBuilder(startPose)
                 .forward(20)
                 .build();
 
-        if (zone == 1) {
+        if (detected.compareTo(1) == 0) {
             drive.followTrajectory(
                     drive.trajectoryBuilder(traj.end(), true)
                             .splineTo(new Vector2d(12, -34), Math.toRadians(180))
                             .build()
             );
-        } else if (zone == 2) {
+        } else if (detected.compareTo(2) == 0) {
             drive.followTrajectory(
                     drive.trajectoryBuilder(traj.end(), true)
                             .forward(10)
                             .build()
             );
-        } else if (zone == 3) {
+        } else if (detected.compareTo(3) == 0) {
             drive.followTrajectory(
                     drive.trajectoryBuilder(traj.end(), true)
                             .splineTo(new Vector2d(58, -34), Math.toRadians(0))
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/components/AprilDetection.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2021 OpenFTC Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.firstinspires.ftc.teamcode.drive.components;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
+public class AprilDetection {
+    OpenCvCamera openCVCamera;
+    AprilPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+    // Tag ID 1,2,3 from the 36h11 family
+    int LEFT = 1;
+    int MIDDLE = 2;
+    int RIGHT = 3;
+
+    AprilTagDetection tagOfInterest = null;
+
+    public AprilDetection(Camera camera, HardwareMap hardwareMap) {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("aprilMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        openCVCamera = OpenCvCameraFactory.getInstance().createWebcam(camera.getWebcamName(), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilPipeline(tagsize, fx, fy, cx, cy);
+
+        openCVCamera.setPipeline(aprilTagDetectionPipeline);
+        openCVCamera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                openCVCamera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+    }
+
+    public void update(Telemetry telemetry) {
+        ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+        if (currentDetections.size() != 0) {
+            boolean tagFound = false;
+
+            for (AprilTagDetection tag : currentDetections) {
+                if (tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT) {
+                    tagOfInterest = tag;
+                    tagFound = true;
+                    break;
+                }
+            }
+
+            if (tagFound) {
+                telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                tagToTelemetry(tagOfInterest, telemetry);
+            } else {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if (tagOfInterest == null) {
+                    telemetry.addLine("(The tag has never been seen)");
+                } else {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest, telemetry);
+                }
+            }
+
+        } else {
+            telemetry.addLine("Don't see tag of interest :(");
+
+            if (tagOfInterest == null) {
+                telemetry.addLine("(The tag has never been seen)");
+            } else {
+                telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                tagToTelemetry(tagOfInterest, telemetry);
+            }
+
+        }
+    }
+
+    public Comparable<Integer> getDetected() {
+        if (tagOfInterest == null) {
+            return null;
+        } else {
+            return tagOfInterest.id;
+        }
+    }
+
+    void tagToTelemetry(AprilTagDetection detection, Telemetry telemetry) {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x * FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y * FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z * FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
+}
